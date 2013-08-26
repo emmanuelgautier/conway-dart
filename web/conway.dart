64fad94860ae7d247e70a059d9be5c4c5174ece2
emmanuelgautier <manu.gautier1394@gmail.com>
2013-08-26 20:01:33 +0200

    add 2 and 3 states feature

--- web/conway.dart
+++ web/conway.dart
@@ -3,29 +3,36 @@ import 'dart:math';
 
 final String black = "#000000";
 final String white = "#FFFFFF";
+final String blue = "#0000FF";
+
+final CanvasElement canvas = query('#conway');
+final CanvasRenderingContext2D ctx = canvas.context2d;
+
+num state;
 
 num canvas_height;
 num canvas_width;
 num height;
 num width;
 num size_pixel;
-final CanvasElement canvas = query('#conway');
-final CanvasRenderingContext2D ctx = canvas.context2d;
 
 List conwayData;
 
 void main() {
-  var container = query(".container");
+  var container = query(".container"),
 
-  canvas.width = window.screen.available.width;
-  canvas.height = window.screen.available.height;
-  
+      choice_two = query("#two"),
+      choice_three = query("#three");
+
+  canvas.height = (window.screen.available.height - window.screen.available.height / 5).toInt();
+  canvas.width = canvas.height;
+
   canvas_width = canvas.width;
   canvas_height = canvas.height;
 
   height = 150;
   width = 150;
-  
+
   size_pixel = canvas_height / height;
 
   conwayData = new List(height);
@@ -36,7 +43,7 @@ void main() {
     conwayData[i] = new List(width);
 
     for(j = 0; j < width; j++){
-      if(r.nextInt(15) == 0){
+      if(r.nextInt(20) == 0){
         conwayData[i][j] = 1;
       } else {
         conwayData[i][j] = 0;
@@ -44,66 +51,82 @@ void main() {
     }
   }
 
+  state = 2;
+
+  choice_two.onClick.listen((MouseEvent me){
+    state = 2;
+  });
+
+  choice_three.onClick.listen((MouseEvent me){
+    state = 3;
+  });
+
   draw();
-  
+
   window.requestAnimationFrame(loop);
 }
 
 void loop(num timer){
   conway();
   draw();
-  
+
   window.requestAnimationFrame(loop);
 }
 
 num numberofNeighbours(num y, num x){
   num n = 0;
-  
-  if(y > 0 && x > 0 && conwayData[y - 1][x - 1] == 1){
+
+  if(y > 0 && x > 0 && conwayData[y - 1][x - 1] != 0){
     n += 1;
   }
-  
-  if(y > 0 && conwayData[y - 1][x] == 1){
+
+  if(y > 0 && conwayData[y - 1][x] != 0){
     n += 1;
   }
-  
-  if(y > 0 && x < (width - 1) && conwayData[y - 1][x + 1] == 1){
+
+  if(y > 0 && x < (width - 1) && conwayData[y - 1][x + 1] != 0){
     n += 1;
   }
-  
-  if(x < (width - 1) && conwayData[y][x + 1] == 1){
+
+  if(x < (width - 1) && conwayData[y][x + 1] != 0){
     n += 1;
   }
-  
-  if(x < (width - 1) && y < (height - 1) && conwayData[y + 1][x + 1] == 1){
+
+  if(x < (width - 1) && y < (height - 1) && conwayData[y + 1][x + 1] != 0){
     n += 1;
   }
-  
-  if(y < (height - 1) && conwayData[y + 1][x] == 1){
+
+  if(y < (height - 1) && conwayData[y + 1][x] != 0){
     n += 1;
   }
-  
-  if(y < (height - 1) && x > 0 && conwayData[y + 1][x - 1] == 1){
+
+  if(y < (height - 1) && x > 0 && conwayData[y + 1][x - 1] != 0){
     n += 1;
   }
-  
-  if(x > 0 && conwayData[y][x - 1] == 1){
+
+  if(x > 0 && conwayData[y][x - 1] != 0){
     n += 1;
   }
-  
+
   return n;
 }
 
 void conway(){
   var t, i, j;
-  
+
   for(i = 0; i < height; i++){
     for(j = 0; j < width; j++){
       t = numberofNeighbours(i, j);
-      
+
       if(conwayData[i][j] == 1 && t != 2 && t != 3){
         conwayData[i][j] = 0;
       } else if(conwayData[i][j] == 0 && t == 3){
+        if(state == 2){
+          conwayData[i][j] = 1;
+        } else {
+          conwayData[i][j] = 2; 
+        }
+      } else if(conwayData[i][j] == 2){
         conwayData[i][j] = 1;
       }
     }
@@ -113,15 +136,9 @@ void conway(){
 void draw(){
   for(var i = 0; i < height; i++){
     for(var j = 0; j < width; j++){
-      if(conwayData[i][j] == 0){
-        ctx.fillStyle = white;
-      } else {
-        ctx.fillStyle = black;
-      }
-      
+      ctx.fillStyle = ((conwayData[i][j] == 0) ? white : (conwayData[i][j] == 1 ? black : blue));
+
       ctx.fillRect(i * size_pixel, j * size_pixel, size_pixel, size_pixel);
     }
   }
 }
-
-
